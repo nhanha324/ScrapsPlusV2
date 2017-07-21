8d08bc015a57238beb128ca42a23d0407b51c086
Nhan <pietro324@gmail.com>
2017-07-21 02:17:18 +0000

    views folder

--- ScrapsPlusV2/wwwroot/js/app.js
+++ ScrapsPlusV2/wwwroot/js/app.js
@@ -2,8 +2,9 @@ var app = angular.module("ScrapsPlusV2" [ngRoute]);
 app.config(function($routeProvider) {
     $routeProvider
     .when("/", {
-        templateUrl : "home.html"
+        templateUrl : "../Views/home.html"
         controller: homeController
+        
     })
     .when("/red", {
         templateUrl : "red.htm"
